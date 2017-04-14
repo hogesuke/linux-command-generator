9df574a798c64d5137976c502192145abc42309e
hogesuke <miyado@gmail.com>
2017-04-14 13:07:23 +0900

    Inputでcommandを受け取るように修正

--- src/app/commands/command/command.component.ts
+++ src/app/commands/command/command.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Command } from './command';
 
 @Component({
   selector: 'app-command',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class CommandComponent {
-
+  @Input() command: Command;
 }
