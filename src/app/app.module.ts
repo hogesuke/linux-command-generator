18d28f0c1d67b706aebbef09645fa671b56f294b
hogesuke <miyado@gmail.com>
2017-04-29 23:28:33 +0900

    オプション追加時のアニメーションを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
@@ -23,6 +24,7 @@ import { ArgumentsExpressionComponent } from './commands/command/arguments-expre
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     RouterModule.forRoot([
