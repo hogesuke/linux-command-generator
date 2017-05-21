6d62f7de6f483df3c19661e0de8020180982357f
hogesuke <miyado@gmail.com>
2017-05-22 00:03:33 +0900

    初期ロード時のアニメーションを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { CommandService } from './shared/command.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.scss' ]
 })
 
 export class AppComponent implements AfterViewInit {
